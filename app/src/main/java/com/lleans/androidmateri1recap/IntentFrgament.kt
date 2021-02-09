c7e343c8621fa2b4f1cced76a31e05fe2238ffb0
lleans <ahmadunnail@gmail.com>
2021-02-09 11:27:49 +0700

    Intent-Layout

---
+++ app/src/main/java/com/lleans/androidmateri1recap/IntentFrgament.kt
@@ -0,0 +1,43 @@
+package com.lleans.androidmateri1recap
+
+import android.content.Intent
+import android.nfc.NfcAdapter.EXTRA_DATA
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class IntentFrgament : Fragment(), View.OnClickListener {
+    lateinit var navController: NavController
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intent_frgament, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.simpan).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.simpan -> {
+                val MoveData = Intent(activity, TargetActivity::class.java)
+                val editText = view?.findViewById(R.id.edtNama) as EditText
+                MoveData.putExtra(TargetActivity.EXTRA_DATA, editText.text.toString())
+                startActivity(MoveData)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/lleans/androidmateri1recap/RelativerFragment.kt
@@ -0,0 +1,35 @@
+package com.lleans.androidmateri1recap
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class RelativerFragment: Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_relative, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next2).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.next2 -> navController!!.navigate(R.id.action_relativerFragment_to_intentFrgament)
+        }
+    }
+}
